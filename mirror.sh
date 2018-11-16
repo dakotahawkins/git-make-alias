fcf70576e45cb59851ce993bd2d4ed070c6c9e57
Dakota Hawkins <dakotahawkins@gmail.com>
2018-11-16 15:07:59 -0500

    fix mirror source remote + cleanup

--- mirror.sh
+++ mirror.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
 main() {
-    local force_opt=
+    declare -a force_opts
+    local non_ff_opt=
     local remote_opts=0
     local source_remote=
     local dest_remote=
     while [[ $# -gt 0 ]]; do
         case "$1" in
             -f|--force)
-                [[ "$remote_opts" -eq 0 ]] && force_opt="--force --prune" || error_exit "Force option must be the first option"
+                [[ "$remote_opts" -eq 0 ]] && {
+                    force_opts=(--force --prune)
+                    non_ff_opt=+
+                } || error_exit "Force option must be the first option"
                 shift
                 ;;
-            -*)
+            [-]*)
                 error_exit "Unexpected option: \"$1\""
                 ;;
             *)
-                [[ "$remote_opts" -eq 0 ]] && {
+                if [[ "$remote_opts" -eq 0 ]]; then
                     remote_opts=1
                     source_remote="$1"
-                } || [[ "$remote_opts" -eq 1 ]] && {
+                elif [[ "$remote_opts" -eq 1 ]]; then
                     remote_opts=2
                     dest_remote="$1"
-                } || error_exit "Unexpected option: \"$1\""
+                else
+                    error_exit "Unexpected option: \"$1\""
+                fi
                 shift
                 ;;
         esac
     done
 
-    git fetch "$source_remote" || error_exit
-    git push "$force_opt" "$dest_remote" refs/remotes/origin/*:refs/heads/* refs/heads/*:refs/heads/* refs/tags/*:refs/tags/* || error_exit
+    git fetch $source_remote || error_exit
+    git push "${force_opts[@]}" $dest_remote \
+        ${non_ff_opt}refs/remotes/$source_remote/*:refs/heads/* \
+        ${non_ff_opt}refs/heads/*:refs/heads/* \
+        ${non_ff_opt}refs/tags/*:refs/tags/* || error_exit
 }
 
 error_exit() {

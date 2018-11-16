2ab634550f9c0a94fc7d5cbe90656514bec1af82
Dakota Hawkins <dakotahawkins@gmail.com>
2018-11-16 15:07:55 -0500

    Added/updated mirror.sh
    M	mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
 main() {
-    ################################
-    # ADD ALIAS FUNCTIONALITY HERE #
-    ################################
-    :
+    local force_opt=
+    local remote_opts=0
+    local source_remote=
+    local dest_remote=
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            -f|--force)
+                [[ "$remote_opts" -eq 0 ]] && force_opt="--force --prune" || error_exit "Force option must be the first option"
+                shift
+                ;;
+            -*)
+                error_exit "Unexpected option: \"$1\""
+            *)
+                [[ "$remote_opts" -eq 0 ]] && {
+                    remote_opts=1
+                    source_remote="$1"
+                } || [[ "$remote_opts" -eq 1 ]] && {
+                    remote_opts=2
+                    dest_remote="$1"
+                } || error_exit "Unexpected option: \"$1\""
+                shift
+                ;;
+        esac
+    done
+
+    git fetch "$source_remote" || error_exit
+    git push "$force_opt" refs/remotes/origin/*:refs/heads/* refs/heads/*:refs/heads/* refs/tags/*:refs/tags/* || error_exit
 }
 
 error_exit() {
